e4c17aec11ebab99cec0338e32b627725e155022
Kamryn <kamclaridge@gmail.com>
2014-02-26 20:04:51 -0800

    Completed Lab 3!

--- static/js/imgrr.js
+++ static/js/imgrr.js
@@ -1,13 +1,27 @@
 // Problem 2 (Peekaboo) ------------------------------------------------------
-// WRITE CODE HERE
+    $('#toggle_img').click(function() {
+    	if ($('#toggle_img').text() === "Go Away!") {
+  $('#main_img').hide();
+  $('#toggle_img').text('Come Back!')
+} else {
+  $('#main_img').show();
+  $('#toggle_img').text('Go Away!')
+}
+
+});
 
 // Problem 3 (Swap Em) -----------------------------------------------
-// WRITE CODE HERE
+$('#change_img').click(function() {
+  $('#main_img').attr('src', '/static/img/' + $('#new_img_file').val());
+});
 
 // Problem 4 (Find the Source) -------------------------------------------------
 $('.clickable').click(function() {
-  // WRITE CODE HERE
+	alert($(this).attr('src'));
+  $('#main_img').attr('src', '/static/img/'+ $('#new_img_file').val());
 });
 
 // Problem 5 (Imgrr) -------------------------------------------------
-// WRITE CODE HERE
+$('.click').click(function() {
+  $('#main_img2').attr('src', $(this).attr('src'));
+});
