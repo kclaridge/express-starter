3ffb49dd0aac4be8b3fc5c662201a42d7257c04a
Kamryn <kamclaridge@gmail.com>
2014-04-02 19:36:04 -0700

    Balls move!

--- static/js/chain_reaction.js
+++ static/js/chain_reaction.js
@@ -16,18 +16,24 @@ var balls = [];
   var b0 = {
     x: 100,
     y: 100,
+    vx: .04,
+    vy: .04,
     radius: 10
   };
 
     var b1 = {
     x: 300,
     y: 100,
+    vx: .04,
+    vy: .04,
     radius: 20
   };
 
     var b2 = {
     x: 500,
     y: 300,
+    vx: .04,
+    vy: .04,
     radius: 30
   };
 
@@ -35,12 +41,22 @@ var balls = [];
   balls.push(b1);
   balls.push(b2);
 
-
   // Run an interation of the game
   var updateGame = function() {
+    for (var i = 0; i < balls.length; i++) {
+    balls[i].x += balls[i].vx;
+    balls[i].y += balls[i].vy;
+    if ((balls[i].x >= canvas.width-balls[i].radius) || (balls[i].x <= balls[i].radius)) {
+      balls[i].vx = -balls[i].vx;
+    } 
+    if ((balls[i].y >= canvas.height-balls[i].radius || (balls[i].y <= balls[i].radius))) {
+      balls[i].vy = - balls[i].vy;} 
+    };
     // PUT STUFF HERE
+    context.clearRect(0,0, canvas.width, canvas.height);
     for (var i = 0; i < balls.length; i++) {
         drawCircle(balls[i].x,balls[i].y,balls[i].radius, 'purple');
+        setTimeout(updateGame, 10);
 };
 };
 
